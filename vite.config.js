2311ac88b9de7d893dc00e06b1bc3a9b9089d1de
Clara Nuoskala <clara.nuoskala@protonmail.com>
2024-08-21 00:39:38 +0300

    update link of github page

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://nuoskala.github.io/responsive_app_tuto/",
+  base: "https://nuoskala.github.io/responsive_mock_app/",
 })
