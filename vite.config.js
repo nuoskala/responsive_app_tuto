f3caf6ac4ec48fa0e4e89f9524ed268b9a0f76ba
Clara Nuoskala <clara.nuoskala@protonmail.com>
2024-08-20 22:56:14 +0300

    add base in vite.config file

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://nuoskala.github.io/responsive_app_tuto/",
 })
